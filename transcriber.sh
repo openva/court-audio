15f9e1ece695738f71b0b134be3081b94151ac22
Waldo Jaquith <waldo@jaquith.org>
2023-03-03 18:58:57 -0500

    Create a transcription script

---
+++ transcriber.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set +x
+
+# Make sure that an input filename has been provided
+if [ "$1" == '' ]; then
+    echo "No filename provided"
+    exit 1
+fi
+
+# Use a better variable name
+MP3="$1"
+
+# Make sure the file exists
+if [ ! -f "$MP3" ]; then
+    echo "File does not exist"
+    exit 1
+fi
+
+# Save the root filename
+FILENAME=${MP3/.mp3/}
+
+# Convert the MP3 to a WAV
+ffmpeg -i "$FILENAME".mp3 -ar 16000 "$FILENAME".wav || exit 1
+
+# Generate the transcript
+./whisper -m models/ggml-base.en.bin -f "$FILENAME".wav --output-srt --output-file "$FILENAME"
+
+# Delete the WAV
+rm -f "$FILENAME".wav
